5b8715b3d25f2bafca30d974680674aca6bb66ff
Dhruv Babani <120258006+dbabani@users.noreply.github.com>
2024-10-26 16:12:26 -0300

    fix/Listagem de Itens para fluxo de Pedido

--- src/main/java/br/com/sgdrs/repository/ItemRepository.java
+++ src/main/java/br/com/sgdrs/repository/ItemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.sgdrs.domain.Item;
 
-public interface ItemRepository extends JpaRepository<Item,UUID> {  
+public interface ItemRepository extends JpaRepository<Item, UUID> { 
 }
