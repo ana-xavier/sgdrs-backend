e2f2ded1c05161a8630cd4d2fc72a5943bb7c2fc
VitorCaus <vitor50033@outlook.com>
2024-11-21 18:54:15 -0300

    :wrench: updated webConfig to connect with localhost:3000

--- src/main/java/br/com/sgdrs/config/WebConfig.java
+++ src/main/java/br/com/sgdrs/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Especifique a origem permitida
+                .allowedOrigins("http://localhost:3000") // Especifique a origem permitida
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
